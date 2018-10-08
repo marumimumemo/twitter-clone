2b44cb8d33ed2fdd4672806e5fd9d11db62aece9
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-10-08 20:56:21 +0900

    edit tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,17 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body).merge(user_id: current_user.id)
+  end
 end
