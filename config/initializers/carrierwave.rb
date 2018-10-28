a0736dc071ec05fd6a1154e500abd247aa54bc03
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-10-28 21:13:34 +0900

    edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'saotoshimarumoto'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/saotoshimarumoto'
+  config.fog_directory  = 'twitter-clone-s3'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/twitter-clone-s3'
 end
