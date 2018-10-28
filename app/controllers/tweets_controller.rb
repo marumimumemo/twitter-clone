9bf6af803b3748c6f699ffab2d0a8e7eca0e7e73
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-10-28 12:21:00 +0900

    edit tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,15 +3,22 @@ class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
     @tweets = Tweet.includes(:user).order("created_at DESC")
+    4.times { @tweet.images.build }
   end
 
   def create
-    Tweet.create(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def tweet_params
-    params.require(:tweet).permit(:body).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:body, images_attributes: [:image]).merge(user_id: current_user.id)
   end
+
 end
