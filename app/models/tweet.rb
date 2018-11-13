f21a8e587440b3e7579fcfa1fe31f330434f0988
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-11-13 15:48:16 +0900

    app/models/tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   mount_uploader :image, ImageUploader
   accepts_nested_attributes_for :images
 end
