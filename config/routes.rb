e058dca7b9d6df84f5371d7529df5b026127782a
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-11-24 17:38:24 +0900

    edit config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root  'tweets#index'
   devise_for :users
-  resources :tweets, only: [:index, :create, :destroy]
+  resources :tweets, only: [:index, :create, :destroy, :show]
   resources :users, only: [:edit, :update, :destroy, :show]
   resources :images
 end
