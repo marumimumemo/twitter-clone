725981786026ebe2b2208bb0eaa2c6d808e963ad
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-12-16 17:42:36 +0900

    edit likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,23 @@
 class LikesController < ApplicationController
+  before_action :set_tweet
   rescue_from ActionController::RedirectBackError, with: :redirect_to_default
 
   def create
     @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
-    @likes = Like.where(tweet_id: params[:tweet_id])
-    @tweets = Tweet.all
     redirect_to :back
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
-    @like.destroy
-    @likes = Like.where(tweet_id: params[:tweet_id])
-    @tweets = Tweet.all
+    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    like.destroy
     redirect_to :back
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 
   def redirect_to_default
     redirect_to root_path
